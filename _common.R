c69ef48eec400710a8bc078ee40aaaf8e5de1279
jyjuni <yj2682@columbia.edu>
2021-12-12 23:42:25 -0500

    publish v1

--- _common.R
+++ _common.R
@@ -3,3 +3,14 @@ knitr::opts_chunk$set(
   message = FALSE,
   warning = FALSE
 )
+library(GGally)
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(prob)
+library(hexbin)
+library(tidyverse)
+library(naniar)
+library(patchwork)
+library(ggcorrplot)
+library(ggridges)
